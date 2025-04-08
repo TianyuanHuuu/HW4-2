fa4e40c70bd396185429f58615639a38cde88de4
TianyuanHuuu <160672802+TianyuanHuuu@users.noreply.github.com>
2025-04-07 22:02:28 -0400

    Update findBlockNonce.py

--- findBlockNonce.py
+++ findBlockNonce.py
@@ -1,56 +1,39 @@
-#!/bin/python
-import hashlib
-import os
-import random
-
-
 def mine_block(k, prev_hash, transactions):
     """
-        k - Number of trailing zeros in the binary representation (integer)
-        prev_hash - the hash of the previous block (bytes)
-        rand_lines - a set of "transactions," i.e., data to be included in this block (list of strings)
+    k - Number of trailing zeros in the binary representation (integer)
+    prev_hash - the hash of the previous block (bytes)
+    transactions - a list of "transactions," i.e., strings to be included in this block
 
-        Complete this function to find a nonce such that 
-        sha256( prev_hash + rand_lines + nonce )
-        has k trailing zeros in its *binary* representation
+    Find a nonce such that:
+        sha256( prev_hash + transactions + nonce )
+    has k trailing zeros in its *binary* representation
     """
     if not isinstance(k, int) or k < 0:
         print("mine_block expects positive integer")
         return b'\x00'
 
-    # TODO your code to find a nonce here
+    # Convert the list of transaction strings to a single byte string
+    tx_data = ''.join(transactions).encode('utf-8')
+
+    nonce_int = 0
+    while True:
+        # Convert integer nonce to bytes
+        nonce = str(nonce_int).encode('utf-8')
+
+        # Concatenate prev_hash + tx_data + nonce
+        block_data = prev_hash + tx_data + nonce
+
+        # Hash the block
+        block_hash = hashlib.sha256(block_data).digest()
+
+        # Convert to binary representation
+        hash_binary = bin(int.from_bytes(block_hash, byteorder='big'))
+
+        # Check if the last k bits are zeros
+        if hash_binary[-k:] == '0' * k:
+            break
+
+        nonce_int += 1
 
     assert isinstance(nonce, bytes), 'nonce should be of type bytes'
     return nonce
-
-
-def get_random_lines(filename, quantity):
-    """
-    This is a helper function to get the quantity of lines ("transactions")
-    as a list from the filename given. 
-    Do not modify this function
-    """
-    lines = []
-    with open(filename, 'r') as f:
-        for line in f:
-            lines.append(line.strip())
-
-    random_lines = []
-    for x in range(quantity):
-        random_lines.append(lines[random.randint(0, quantity - 1)])
-    return random_lines
-
-
-if __name__ == '__main__':
-    # This code will be helpful for your testing
-    filename = "bitcoin_text.txt"
-    num_lines = 10  # The number of "transactions" included in the block
-
-    # The "difficulty" level. For our blocks this is the number of Least Significant Bits
-    # that are 0s. For example, if diff = 5 then the last 5 bits of a valid block hash would be zeros
-    # The grader will not exceed 20 bits of "difficulty" because larger values take to long
-    diff = 20
-
-    transactions = get_random_lines(filename, num_lines)
-    nonce = mine_block(diff, transactions)
-    print(nonce)
